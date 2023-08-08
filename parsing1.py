0e981935b0672dcd68e4b4e5d5f3cac07d38bc76
이수민 <sumin510@naver.com>
2023-08-09 08:59:40 +0900

    9일 아침

--- parsing1.py
+++ parsing1.py
@@ -1,15 +1,15 @@
 # 문자열에서 알파벳과 숫자 구분하여 추출하기 - import re 사용하기
 
-# import re
-# sent = input()
-# name_list = re.findall("[a-zA-Z]+", sent)
-# num_list = re.findall(r'\d+', sent)
-#
-# result = []
-# for i, j in zip(name_list, num_list):
-#     ans = i + '#' + str(int(j) + 17)
-#     result.append(ans)
-# print(*result, sep='\n')
+import re
+sent = 'AB110CDEF11G4H5'
+name_list = re.findall("[a-zA-Z]+", sent)
+num_list = re.findall(r'\d+', sent)
+
+result = []
+for i, j in zip(name_list, num_list):
+    ans = i + '#' + str(int(j) + 17)
+    result.append(ans)
+print(*result, sep='\n')
 
 
 
@@ -33,8 +33,33 @@
 #
 # print(names, numbers)
 
+# stack 이용
+sent = 'AB110CDEF11G4H5'
+left = []
+names = []
+numbers = []
 
+start = 0
+end = len(sent) - 1
+while start <= end:
+    left.append(sent[start])
+    start += 1
+    if sent[start - 1].isalpha():  # 스택 최상단 문자열이 대문자일 때
+        if sent[start].isdecimal():  # 스택에 쌓일 데이터가 숫자라면
+            for i in range(len(left)): # 스택에 있는 데이터를 옮기고 삭제
+                temp = []
+                temp.append(left[i])
+                names.append(temp)
+                left.clear()
+    elif sent[start - 1].isdecimal():  # 스택 최상단 문자열이 숫자일 때
+        if sent[start].isalpha():  # 스택에 쌓일 데이터가 대문자라면
+            for i in range(len(left)):  # 스택에 있는 데이터를 옮기고 삭제
+                temp = []
+                temp.append(left[i])
+                numbers.append(temp)
+                left.clear()
+    if start == end:
+        numbers.append(left)
 
-
-
+print(names, numbers)
 
--- str_ex2.py
+++ str_ex2.py
@@ -1,4 +1,27 @@
 # 폭발 문자열
 s = input()
 bs = input()
+left = []
 
+start = 0
+end = len(s) - 1
+
+while start <= end:  # start = end인 경우는 전체 문자열의 마지막 요소를 스택 최상단에 넣는 경우
+    # 스택에 비교할 문자열 차례로 쌓기
+    tof = True
+    left.append(s[start])
+    start += 1  # 리스트의 다음 요소를 스택에 쌓을 준비
+    # 스택 상단의 문자열과 찾고자 하는 문자열을 비교
+    if len(left) >= len(bs):  # 스택의 길이가 찾고자 하는 문자열의 길이와 같거나 큰 경우에만 비교
+        for i in range(len(bs)):  # 매 비교마다 찾는 문자열의 길이만큼 비교작업 발생
+            if bs[i] != left[len(left) - len(bs) + i]:  # 비교했으나 일치하지 않는 경우
+                tof = False  # 찾는 문자열의 길이만큼 비교를 하는데 하나라도 일치하지 않으면
+                break  # 아무것도 지우지 않고 다시 스택 상단에 문자열을 쌓는 단계로 돌아감
+            elif tof:  # 문자열을 찾은 경우
+                for i in range(len(bs)):  # 찾는 문자열의 길이 만큼
+                    left.pop()  # 스택 최상단의 데이터 삭제
+
+if len(left) == 0:
+    print('FRULA')
+else:
+    print(*left, sep='')
